7c0f599295a90b4842247de7459a0ce6bcec5060
Jonathan <jonaaldas@gmail.com>
2021-12-29 18:35:43 -0500

    added logic to the submit button and made the delete button worked

--- app.js
+++ app.js
@@ -12,21 +12,28 @@ a.addEventListener('change', function() {
   lang = this.value;
 });
 
+//buttons
+
+
 
 // values
 let titleInputValue,
     textAreaValaue;
-    let lang = 'javascript'
+    let lang = 'javascript';
+
+    let codeCont,condeInner,h3,btn1,btn2,pre,code;
+
 
 function clearButton(){
   document.querySelector('.tite-input').value = '';
   document.querySelector('textarea').value = '';
-  titleInputValue = '';
-  textAreaValaue = '';
+  titleInputValue = null;
+  textAreaValaue = null;
 }
 
 function getTitleInputValue(){
   titleInputValue = this.value;
+  console.log(typeof titleInputValue)
 }
 
 function getTextAreaValue(){
@@ -35,41 +42,51 @@ function getTextAreaValue(){
 
 
 function submitButton(){
-  if(titleInputValue === undefined && textAreaValaue === undefined){
+  if(typeof titleInputValue != 'string' && typeof textAreaValaue != 'string'){
     alert('Do not leave fields empty')
-  } else {
-    let codeCont = document.querySelector(".code-cont");
-
-    let condeInner = document.createElement('div');
-    condeInner.className = 'code-inner';
-    codeCont.appendChild(condeInner)
-    
-    let h3 = document.createElement("h3");
-    h3.className = 'code-title'
-    h3.innerText = titleInputValue;
-    
-    let btn1 = document.createElement("button");
-    btn1.className = 'edit-btn btn btn-success'
-    btn1.innerText = 'Edit';
-    let btn2 = document.createElement("button");
-    btn2.className = 'delete-btn btn btn-success'
-    btn2.innerText = 'Delete';
-    
-    let pre = document.createElement("pre");
-    let code = document.createElement("code");
-    pre.appendChild(code);
-    
-    code.className = `snippet language-${lang}`;
-    code.innerText = textAreaValaue;
-    condeInner.appendChild(pre);
-    condeInner.appendChild(h3);
-    condeInner.appendChild(btn1);
-    condeInner.appendChild(btn2);
-
-    Prism.hooks.add("before-sanity-check", function (env) {
-      env.code = env.element.innerText;
-      console.log(env.code)
-      });
-      Prism.highlightElement(code);
+  } else if(typeof titleInputValue === 'string' && typeof textAreaValaue === 'string'){
+    parseHtml();
   } 
 }
+
+function deleteCode(){
+  this.parentNode.remove()
+}
+
+function parseHtml(){
+  console.log(titleInputValue,textAreaValaue)
+  codeCont = document.querySelector(".code-cont");
+
+  condeInner = document.createElement('div');
+  condeInner.className = 'code-inner';
+  codeCont.appendChild(condeInner)
+  
+  h3 = document.createElement("h3");
+  h3.className = 'code-title'
+  h3.innerText = titleInputValue;
+  
+  btn1 = document.createElement("button");
+  btn1.className = 'edit-btn btn btn-success'
+  btn1.innerText = 'Edit';
+  btn2 = document.createElement("button");
+  btn2.className = 'delete-btn btn btn-success';
+  btn2.onclick = deleteCode;
+  btn2.innerText = 'Delete';
+  
+  pre = document.createElement("pre");
+  code = document.createElement("code");
+  pre.appendChild(code);
+  
+  code.className = `snippet language-${lang}`;
+  code.innerText = textAreaValaue;
+  condeInner.appendChild(pre);
+  condeInner.appendChild(h3);
+  condeInner.appendChild(btn1);
+  condeInner.appendChild(btn2);
+
+  Prism.hooks.add("before-sanity-check", function (env) {
+    env.code = env.element.innerText;
+    });
+    Prism.highlightElement(code);
+}
+
