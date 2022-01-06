657c4d177b1cb776713e1a3b7af5572818f80524
Jonathan aldas <jonathanaldas@pool-100-0-0-61.bstnma.fios.verizon.net>
2022-01-06 08:41:29 -0500

    made the replace feature worked

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ dropDown.addEventListener('change', function() {
 let titleInputValue,
     textAreaValaue;
     let lang = 'javascript';
+    let dataValue = 0;
 let codeSnippets = [];
 
 //Dom elemenets created
@@ -55,10 +56,13 @@ function submitButton(){
   if(typeof titleInputValue != 'string' && typeof textAreaValaue != 'string'){
     alert('Do not leave fields empty')
   } else if(typeof titleInputValue === 'string' && typeof textAreaValaue === 'string'){
-    parseHtml(titleInputValue,textAreaValaue);
+    dataValue++;
+    parseHtml(titleInputValue,textAreaValaue,dataValue);
+    //make an HTMLCollection of all of the data 
     let allData = document.querySelector('.code-cont').children;
-    console.log(allData)
-    // codeSnippets.push();
+    //speard it into the the array
+    codeSnippets = [...allData];
+    // console.log(codeSnippets)
   } 
 }
 
@@ -70,13 +74,13 @@ function deleteCodeBlock(){
 }
 
 // Creates dom elements and addes them to the DOM and Prsismjs parese them 
-function parseHtml(title, textArea, ){
+function parseHtml(title, textArea, idIdentifier){
 
   codeCont = document.querySelector(".code-cont");
 
   condeInner = document.createElement('div');
   condeInner.className = `code-inner`;
-  // condeInner.setAttribute(`data-value`, `${idIdentifier}`)
+  condeInner.setAttribute(`data-value`, `${idIdentifier}`)
   codeCont.appendChild(condeInner)
   
   h3 = document.createElement("h3");
@@ -112,46 +116,105 @@ function parseHtml(title, textArea, ){
 
 
 // edit code block
-function editCodeBlock(){
+// function editCodeBlock(){
 
+//   let edit1, edit2;
+
+//   let btnSave = document.createElement('button');
+//   btnSave.className = 'btn-save btn btn-success';
+//   btnSave.innerHTML = 'Save';
+
+//   let btnSubmit = document.createElement('button');
+//   btnSubmit.className = 'btn-submit btn btn-success';
+//   btnSubmit.innerHTML = 'Submit';
+  
+  
+  
+//   // console.log(this.parentNode.getAttribute('data-value'));
+//   btnSave.onclick = () => {
+//    if(edit1 === undefined && edit2 === undefined){
+//      alert('please change me ')
+//    } else {
+//       // for(let i in codeSnippets){
+//         // if (codeSnippets[i].getAttribute('data-value') === this.parentNode.getAttribute('data-value')){
+//             console.log(this.parentNode)
+//       // }
+//     //  document.querySelector('.btn-save').remove();
+//     //  document.querySelector('.buttons').appendChild(btnSubmit);
+//     //  document.querySelector('.btn-submit').addEventListener('click', submitButton);
+//     //  parseHtml(edit1,edit2,dataValue);
+//     }
+//   }
+
+//   document.querySelector('.btn-submit').remove();
+//   document.querySelector('.buttons').appendChild(btnSave);
+
+
+//   document.querySelector('.tite-input').value = editTextAreavalue;
+//   document.querySelector('textarea').value = editTitleInputValue;
+//   //
+//   document.querySelector('.tite-input').addEventListener('input', function(){
+//     edit1 = this.value;
+//   });
+
+//   document.querySelector('textarea').addEventListener('input', function(){
+//     edit2 = this.value;
+//   });
+// }
+
+
+
+//new edit block 
+
+function editCodeBlock(){
   let edit1, edit2;
+
+  document.querySelector('.tite-input').value = editTextAreavalue;
+  document.querySelector('textarea').value = editTitleInputValue;
+    
+  document.querySelector('.tite-input').addEventListener('input', function(){
+      edit1 = this.value;
+    });
+  
+  document.querySelector('textarea').addEventListener('input', function(){
+      edit2 = this.value;
+    });
+
 
   let btnSave = document.createElement('button');
   btnSave.className = 'btn-save btn btn-success';
   btnSave.innerHTML = 'Save';
 
-  let btnSubmit = document.createElement('button');
-  btnSubmit.className = 'btn-submit btn btn-success';
-  btnSubmit.innerHTML = 'Submit';
-  
-  
-  
-  btnSave.onclick = () => {
-   if(edit1 === undefined && edit2 === undefined){
-     alert('please change me ')
-   } else {
-     document.querySelector('.code-inner').remove();
-     document.querySelector('.btn-save').remove();
-     document.querySelector('.buttons').appendChild(btnSubmit);
-     document.querySelector('.btn-submit').addEventListener('click', submitButton);
-     parseHtml(edit1,edit2);
-    }
-  }
+  // let btnSubmit = document.createElement('button');
+  // btnSubmit.className = 'btn-submit btn btn-success';
+  // btnSubmit.innerHTML = 'Submit';
 
   document.querySelector('.btn-submit').remove();
   document.querySelector('.buttons').appendChild(btnSave);
 
 
-  document.querySelector('.tite-input').value = editTextAreavalue;
-  document.querySelector('textarea').value = editTitleInputValue;
-  //
-  document.querySelector('.tite-input').addEventListener('input', function(){
-    edit1 = this.value;
-  });
 
-  document.querySelector('textarea').addEventListener('input', function(){
-    edit2 = this.value;
-  });
+  // save button
 
+  // console.log(this.parentNode.getAttribute('data-value'));
+  btnSave.onclick = () => {
+  if(edit1 === undefined && edit2 === undefined){
+    alert('please change me ')
+  } else {
+      //  this.parentNode.replaceWith(parseHtml(edit1, edit2))
+      this.parentNode.remove();
+      this.parentNode.replaceWith(parseHtml(edit1, edit2))
+    }
+  }
 }
 
+
+
+// for(let i in codeSnippets){
+  // if (codeSnippets[i].getAttribute('data-value') === this.parentNode.getAttribute('data-value')){
+      // console.log(this.parentNode)
+// }
+//  document.querySelector('.btn-save').remove();
+//  document.querySelector('.buttons').appendChild(btnSubmit);
+//  document.querySelector('.btn-submit').addEventListener('click', submitButton);
+//  parseHtml(edit1,edit2,dataValue);
